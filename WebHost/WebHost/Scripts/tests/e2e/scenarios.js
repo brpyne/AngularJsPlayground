a29b7e8ab538bef9c5d87c48658020918cb89650
Brad Pyne <brpyne@gmail.com>
2013-08-16 17:19:56 -0400

    Sorting on list, added age to phone model

--- WebHost/WebHost/Scripts/tests/e2e/scenarios.js
+++ WebHost/WebHost/Scripts/tests/e2e/scenarios.js
@@ -7,7 +7,9 @@
     });
  
  
-    it('should filter the phone list as user types into the search box', function() {
+    it('should filter the phone list as user types into the search box', function () {
+        
+
       expect(repeater('.phones li').count()).toBe(3);
  
       input('query').enter('nexus');
@@ -15,19 +17,51 @@
  
       input('query').enter('motorola');
       expect(repeater('.phones li').count()).toBe(2);
+        
+
     });
       
+
+
     it('should display the current filter value within an element with id "status"',
-  function () {
-      expect(element('#status').text()).toMatch(/Current filter: \s*$/);
+      function () {
+          
 
-      input('query').enter('nexus');
+          expect(element('#status').text()).toMatch(/Current filter: \s*$/);
 
-      expect(element('#status').text()).toMatch(/Current filter: nexus\s*$/);
+          input('query').enter('nexus');
 
-      //alternative version of the last assertion that tests just the value of the binding
-      using('#status').expect(binding('query')).toBe('nexus');
-  });
+          expect(element('#status').text()).toMatch(/Current filter: nexus\s*$/);
+
+          //alternative version of the last assertion that tests just the value of the binding
+          using('#status').expect(binding('query')).toBe('nexus');
+          
+
+      });
+
+
+
+
+
+    it('should be possible to control phone order via the drop down select box',
+      function () {
+          
+
+          //let's narrow the dataset to make the test assertions shorter
+          input('query').enter('tablet');
+
+          expect(repeater('.phones li', 'Phone List').column('phone.name')).
+              toEqual(["Motorola XOOM\u2122 with Wi-Fi",
+                       "MOTOROLA XOOM\u2122"]);
+
+          select('orderProp').option('Alphabetical');
+
+          expect(repeater('.phones li', 'Phone List').column('phone.name')).
+              toEqual(["MOTOROLA XOOM\u2122",
+                       "Motorola XOOM\u2122 with Wi-Fi"]);
+          
+
+      });
       
       /*
       Add a pause() statement inside of an end-to-end test and rerun it. 
--- WebHost/WebHost/Scripts/tests/unit/controllersSpec.js
+++ WebHost/WebHost/Scripts/tests/unit/controllersSpec.js
@@ -1,16 +1,24 @@
 ﻿describe('PhoneCat controllers', function () {
 
     describe('PhoneListCtrl', function () {
+        var scope, ctrl;
+
+        beforeEach(function () {
+            scope = {},
+            ctrl = new PhoneListCtrl(scope);
+        });
+
 
         it('should create "phones" model with 3 phones', function () {
-            var scope = {},
-                ctrl = new PhoneListCtrl(scope);
-
             expect(scope.phones.length).toBe(3);
+        });
+
+
+        it('should set the default value of orderProp model', function () {
+            expect(scope.orderProp).toBe('age');
         });
     });
 });
-
 /*
 Angular developers prefer the syntax of Jasmine's Behavior-driven Development (BDD) framework when writing tests. 
 Although Angular does not require you to use Jasmine, we wrote all of the tests in this tutorial in Jasmine. 
