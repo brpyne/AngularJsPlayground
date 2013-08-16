097abf8a5aa65c25c7c522bf17e1e15fb584a02b
Brad Pyne <brpyne@gmail.com>
2013-08-16 16:52:02 -0400

    Template for Phone Example, Controller

---
+++ WebHost/WebHost/Scripts/tests/unit/controllersSpec.js
@@ -0,0 +1,18 @@
+﻿describe('PhoneCat controllers', function () {
+
+    describe('PhoneListCtrl', function () {
+
+        it('should create "phones" model with 3 phones', function () {
+            var scope = {},
+                ctrl = new PhoneListCtrl(scope);
+
+            expect(scope.phones.length).toBe(3);
+        });
+    });
+});
+
+/*
+Angular developers prefer the syntax of Jasmine's Behavior-driven Development (BDD) framework when writing tests. 
+Although Angular does not require you to use Jasmine, we wrote all of the tests in this tutorial in Jasmine. 
+You can learn about Jasmine on the Jasmine home page and on the Jasmine wiki.
+*/
